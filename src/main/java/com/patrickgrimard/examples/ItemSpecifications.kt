09212aee0a1d5adcdbcfe5ffe541be1daa7d2f69
MotorFu <727547310@qq.com>
2017-04-19 11:54:26 +0800

    commit

--- src/main/java/com/patrickgrimard/examples/ItemSpecifications.kt
+++ src/main/java/com/patrickgrimard/examples/ItemSpecifications.kt
@@ -4,6 +4,8 @@ import au.com.console.jpaspecificationdsl.greaterThan
 import org.springframework.data.jpa.domain.Specifications
 
 /**
+ *
  * [Item]s with quantity > 0
+ *19IZ2Wd9Xs
  */
 fun hasQuantity(): Specifications<Item> = Item::quantity.greaterThan(0L)
